347a8c713f5af43160c1cbc0c4aa4dcb3e4c6ce7
thananjayan26 <jayanselva26@gmail.com>
2024-07-25 22:25:07 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
+import path from "path"
 
 import authRoutes from "./routes/auth.routes.js"
 import messageRoutes from "./routes/message.routes.js"
@@ -11,6 +12,8 @@ import {app, server} from './socket/socket.js'
 
 const PORT = process.env.PORT || 8000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json()); 
@@ -20,11 +23,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.get("/", (req, res) => {
-    // root route http://localhost:8000/
-    res.send("HEkjbjllo")
-});
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
 
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 
 server.listen(PORT, () => {
     connectToMongoDB()
